559b89812cbe020e2fd832c4c37a062a4233ce34
Andrew <xie.andrew2235@gmail.com>
2019-04-18 15:58:56 -0700

    created schema class, article schema that requires title, link, and note

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //DEPENDENCIES
 var express = require("express");
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var mongojs = require("mongojs");
@@ -7,3 +8,82 @@ var mongojs = require("mongojs");
 //Scraping Models
 var request = require("request");
 var cheerio = require("cheerio");
+
+//Requiring our Note and Article models
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+mongoose.Promise = Promise;
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb: https://git.heroku.com/still-eyrie-18314.git"
+var db = mongoose.connection;
+
+// If there is an error connecting to mongoose, console log errior
+db.on("error", function(error){
+    console.log(error);
+})
+db.once("open", function() {
+	console.log("Mongoose connection success.");
+});
+
+// Routes
+// Scrape route
+app.get("/scrape", function(req, res) {
+    request("https://www.coindesk.com", function(error, response, html) {
+        var $ = cheerio.load(html);
+        $('h3, #content').each(function(i, element) {
+            var result = {};
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
+            var entry = new Article(result);
+            entry.save(function(err, doc) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                // Or log the doc
+                else {
+                    console.log(doc);
+                }
+            });
+        
+          });
+        });
+        res.send("Scrape Complete");
+    });	
+    
+    // This will get the articles we scraped from the mongoDB
+    app.get("/articles", function(req, res) {
+      // Grab every doc in the Articles array
+      Article.find({}, function(error, doc) {
+        // Log any errors
+        if (error) {
+          console.log(error);
+        }
+        // Or send the doc to the browser as a json object
+        else {
+          res.json(doc);
+        }
+      });
+    });
+    
+// Grab by objectID
+app.get("/", function(req, res) {
+
+})
+
+// Post route that creates new note/updates
+app.post("", function(req, res) {
+
+})
+
+var port = process.env.PORT || 3000;
+app.listen(port);
