91038ffdc955412c2c06dde53e71a2b82867dc49
Andrew <xie.andrew2235@gmail.com>
2019-04-18 16:27:57 -0700

    added two get routes: to grab scraped articles from mongodb and to grab an article by objectid that sends to browser as json

--- server.js
+++ server.js
@@ -27,24 +27,24 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb: https://git.heroku.com/st
 var db = mongoose.connection;
 
 // If there is an error connecting to mongoose, console log errior
-db.on("error", function(error){
+db.on("error", function (error) {
     console.log(error);
 })
-db.once("open", function() {
-	console.log("Mongoose connection success.");
+db.once("open", function () {
+    console.log("Mongoose connection success.");
 });
 
 // Routes
 // Scrape route
-app.get("/scrape", function(req, res) {
-    request("https://www.coindesk.com", function(error, response, html) {
+app.get("/scrape", function (req, res) {
+    request("https://www.coindesk.com", function (error, response, html) {
         var $ = cheerio.load(html);
-        $('h3, #content').each(function(i, element) {
+        $('h3, #content').each(function (i, element) {
             var result = {};
             result.title = $(this).children("a").text();
             result.link = $(this).children("a").attr("href");
             var entry = new Article(result);
-            entry.save(function(err, doc) {
+            entry.save(function (err, doc) {
                 // Log any errors
                 if (err) {
                     console.log(err);
@@ -54,34 +54,46 @@ app.get("/scrape", function(req, res) {
                     console.log(doc);
                 }
             });
-        
-          });
+
         });
-        res.send("Scrape Complete");
-    });	
-    
-    // This will get the articles we scraped from the mongoDB
-    app.get("/articles", function(req, res) {
-      // Grab every doc in the Articles array
-      Article.find({}, function(error, doc) {
+    });
+    res.send("Scrape Complete");
+});
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function (req, res) {
+    // Grab every doc in the Articles array
+    Article.find({}, function (error, doc) {
         // Log any errors
         if (error) {
-          console.log(error);
+            console.log(error);
         }
         // Or send the doc to the browser as a json object
         else {
-          res.json(doc);
+            res.json(doc);
         }
-      });
     });
-    
-// Grab by objectID
-app.get("/", function(req, res) {
+});
 
-})
+// Grab an article by ObjectId
+app.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, query finds the matching ID in our db and populate all of the notes associated with it
+    Article.findOne({ "_id": req.params.id })
+        .populate("note")
+        .exec(function (error, doc) {
+            // Log any errors, otherwise send to browser as JSON object
+            if (error) {
+                console.log(error);
+            }
+            else {
+                res.json(doc);
+            }
+        });
+});
+
 
 // Post route that creates new note/updates
-app.post("", function(req, res) {
+app.post("", function (req, res) {
 
 })
 
