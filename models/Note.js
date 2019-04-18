e769c5e55cf3c8379fe8125492c6d50d04132776
Andrew <xie.andrew2235@gmail.com>
2019-04-18 16:09:02 -0700

    created schema for notes

--- models/Note.js
+++ models/Note.js
@@ -2,3 +2,20 @@
 var mongoose = require("mongoose");
 // Create a schema class
 var Schema = mongoose.Schema;
+// Create the Note schema
+var NoteSchema = new Schema({
+
+    title: {
+      type: String
+    },
+
+    body: {
+      type: String
+    }
+  });
+
+  // Create Note model with NoteSchema
+  var Note = mongoose.model("Note", NoteSchema);
+  
+  // Exporting Note model
+  module.exports = Note;
